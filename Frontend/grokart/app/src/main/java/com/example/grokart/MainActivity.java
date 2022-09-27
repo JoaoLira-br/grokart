72fde1aad94e9d2eacf9272639722c2c07ed3292
Joao Lira <lira440440@gmail.com>
2022-09-26 22:11:01 -0500

    creates logo page

--- Frontend/grokart/app/src/main/java/com/example/grokart/MainActivity.java
+++ Frontend/grokart/app/src/main/java/com/example/grokart/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.grokart;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn_login = findViewById(R.id.btn_login);
+        Button btn_register = findViewById(R.id.btn_register);
+
+        //TODO Send user to register page
+        btn_register.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        //TODO Send user to register page
+        btn_login.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    public void sendRegisterPage(View view) {
+
     }
 }
