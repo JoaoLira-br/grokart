3db27d9acc5a14ddf65d65de3b4f2f3c823ab8ff
BagaBBG <70350040+BagaBBG@users.noreply.github.com>
2022-09-27 19:23:15 -0500

    User Repo and User Controller

--- Backend/GroKart Backend/groKart_v1/src/main/java/users/UserController.java
+++ Backend/GroKart Backend/groKart_v1/src/main/java/users/UserController.java
@@ -1,7 +1,32 @@
 package users;
 
-//import
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    //@Autowired
+    //ItemRepository itemRepository;
+
+    private String requestSuccess = "{\"message\":\"success\"}";
+
+    @GetMapping(path = "/users")
+    List<User> getUsersLists(){
+        return userRepository.findAll();
+    }
+
 
 }
