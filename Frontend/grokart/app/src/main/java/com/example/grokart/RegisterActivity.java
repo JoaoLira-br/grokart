79d99b690f735e5958b18c6210d629cc272bac8a
Joao Lira <lira440440@gmail.com>
2022-09-28 13:44:25 -0500

    Adds AppController.java for Volley request queues and const.java for storing paths

--- Frontend/grokart/app/src/main/java/com/example/grokart/RegisterActivity.java
+++ Frontend/grokart/app/src/main/java/com/example/grokart/RegisterActivity.java
@@ -1,7 +1,9 @@
 package com.example.grokart;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.text.Spannable;
@@ -13,7 +15,20 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.URL;
+
 public class RegisterActivity extends AppCompatActivity {
+    EditText et_username, et_password;
+    Boolean isValid = false;
+    JSONObject user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,20 +42,64 @@ public class RegisterActivity extends AppCompatActivity {
         groKart.setSpan(new ForegroundColorSpan(Color.RED),3,groKart.length(), Spannable.SPAN_EXCLUSIVE_INCLUSIVE);
         tv_appName.setText(groKart);
 
-        EditText et_username =  findViewById(R.id.et_username);
-        EditText et_password =  findViewById(R.id.et_password);
+        et_username =  findViewById(R.id.et_username);
+        et_password =  findViewById(R.id.et_password);
 
         Button btn_register = findViewById(R.id.btn_register);
         btn_register.setOnClickListener(new View.OnClickListener() {
 
-            //TODO make sure password and username are filled properly
+
             @Override
             public void onClick(View view) {
                 String username = et_username.getText().toString();
                 String password = et_password.getText().toString();
+                checkInputs();
 
                 //TODO send input to backend and proceed to Home page
+                // with response as Intent Extra
+                if(isValid) {
+
+
+                }
+
             }
         });
+    }
+
+
+    private boolean checkInputs() {
+        int invalidCounter = 0;
+        if (et_username.length() == 0) {
+            et_username.setError("Username is required");
+            invalidCounter++;
+        }
+        if (et_password.length() == 0) {
+            et_password.setError("Password is required");
+            invalidCounter++;
+        } else if (et_password.length() < 8) {
+            et_password.setError("Password must be minimum 8 characters");
+            invalidCounter++;
+        }
+        isValid = invalidCounter == 0;
+        return isValid;
+        // after all validation return true.
+    }
+
+    private JSONObject createUser(String username, String password){
+        user = new JSONObject();
+        try {
+            user.put("username", username);
+            user.put("password", password);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return user;
+    }
+
+
+
+    public void sendToHome(View view) {
+       // Intent intent = new Intent(this, HomeActivity.class);
+        //startActivity(intent);
     }
 }
