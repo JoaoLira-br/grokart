ac7187bbc7a55e3ac12ad6a6779488e4cd83183c
Mattie <mrm4@iastate.edu>
2022-09-30 15:00:34 -0500

    made some changes

--- Experiments/Mattie/tutorials-android_unit2_1_volley/AndroidVolley/app/src/main/java/com/example/sumon/androidvolley/utils/Const.java
+++ Experiments/Mattie/tutorials-android_unit2_1_volley/AndroidVolley/app/src/main/java/com/example/sumon/androidvolley/utils/Const.java
@@ -3,6 +3,10 @@ package com.example.sumon.androidvolley.utils;
 public class Const {
 	public static final String URL_JSON_OBJECT = "https://jsonplaceholder.typicode.com/users/1";
 	public static final String URL_JSON_ARRAY = "https://jsonplaceholder.typicode.com/users";
-	public static final String URL_STRING_REQ = "https://jsonplaceholder.typicode.com/users/1";
-	public static final String URL_IMAGE = "http://sharding.org/outgoing/temp/testimg3.jpg";
+	//public static final String URL_STRING_REQ = "https://jsonplaceholder.typicode.com/users/1";
+	//http://date.jsontest.com/
+	public static final String URL_STRING_REQ = "http://date.jsontest.com/";
+	//public static final String URL_IMAGE = "http://sharding.org/outgoing/temp/testimg3.jpg";\
+	public static final String URL_IMAGE = "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/640px-Image_created_with_a_mobile_phone.png";
+	//
 }
