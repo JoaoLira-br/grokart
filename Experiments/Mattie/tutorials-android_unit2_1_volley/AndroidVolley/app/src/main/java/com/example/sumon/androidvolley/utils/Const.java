6cc0118d7ba20a3151f8ac145881b4e436e158ab
Mattie <mrm4@iastate.edu>
2022-10-01 14:06:52 -0500

    postman mock server

--- Experiments/Mattie/tutorials-android_unit2_1_volley/AndroidVolley/app/src/main/java/com/example/sumon/androidvolley/utils/Const.java
+++ Experiments/Mattie/tutorials-android_unit2_1_volley/AndroidVolley/app/src/main/java/com/example/sumon/androidvolley/utils/Const.java
@@ -1,7 +1,8 @@
 package com.example.sumon.androidvolley.utils;
 
 public class Const {
-	public static final String URL_JSON_OBJECT = "https://jsonplaceholder.typicode.com/users/1";
+	//public static final String URL_JSON_OBJECT = "https://jsonplaceholder.typicode.com/users/1";
+	public static final String URL_JSON_OBJECT = "https://eb90d981-fc0b-42ec-ad43-3cfec437d3ed.mock.pstmn.io/Object/"; //postman mock server
 	public static final String URL_JSON_ARRAY = "https://jsonplaceholder.typicode.com/users";
 	//public static final String URL_STRING_REQ = "https://jsonplaceholder.typicode.com/users/1";
 	//http://date.jsontest.com/
