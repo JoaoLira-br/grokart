e166efbfec23fbc308fd5d3fc745c709cdce5d90
Mattie <mrm4@iastate.edu>
2022-10-05 17:41:01 -0500

    json get user working (displays user name at top of page)

--- Frontend/grokart/app/src/main/java/com/example/grokart/EditProfileActivity.java
+++ Frontend/grokart/app/src/main/java/com/example/grokart/EditProfileActivity.java
@@ -32,6 +32,7 @@ public class EditProfileActivity extends AppCompatActivity {
     private RequestQueue mQueue;
     private String TAG = RegisterActivity.class.getSimpleName();
     private TextView msgResponse;
+    private String username = "";
     // These tags will be used to cancel the requests
     private String tag_json_obj = "jobj_req", tag_json_arry = "jarray_req";
 
@@ -49,12 +50,17 @@ public class EditProfileActivity extends AppCompatActivity {
 
         Button btn_editProfile = findViewById(R.id.btn_editProfile);
 
+
+
         mQueue = Volley.newRequestQueue(this);
+        jsonGetUser();
 
         btn_editProfile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                jsonTest();
+                //jsonTest();
+                jsonUpdateUser();
+
             }
         });
     }
@@ -76,6 +82,74 @@ public class EditProfileActivity extends AppCompatActivity {
                 error.printStackTrace();
             }
         });
+        mQueue.add(request);
+    }
+
+    private void jsonGetUser() {
+        String url = "https://eb90d981-fc0b-42ec-ad43-3cfec437d3ed.mock.pstmn.io/user/1";
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d(TAG, response.toString());
+                        try {
+                            username = response.getString("username");
+                            msgResponse.setText("hello " + username);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+        mQueue.add(request);
+    }
+    private void jsonUpdateUser() {
+        String url = "https://eb90d981-fc0b-42ec-ad43-3cfec437d3ed.mock.pstmn.io/user/" + username;
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.PUT, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d(TAG, response.toString());
+                        try {
+                            msgResponse.setText(response.getString("message"));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                headers.put("Content-Type", "application/json");
+                return headers;
+            }
+
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("name", et_name.getText().toString());
+                params.put("email", et_email.getText().toString());
+                params.put("phone", et_phone.getText().toString());
+                params.put("preferredStore", et_preferredStore.getText().toString());
+
+                return params;
+            }
+        };
         mQueue.add(request);
     }
 }
