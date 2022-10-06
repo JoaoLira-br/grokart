9d6d4401cac306bf1277fa1230baea3d3078e7d5
Joao Lira <lira440440@gmail.com>
2022-10-06 09:35:50 -0500

    Updates roundtrip communication methods and URLs in RegisterActivity.java and Const.java

--- Frontend/grokart/app/src/main/java/com/example/grokart/utils/Const.java
+++ Frontend/grokart/app/src/main/java/com/example/grokart/utils/Const.java
@@ -6,5 +6,8 @@ public class Const {
 //	public static final String URL_STRING_REQ = "https://dummyjson.com/users/search?q=John";
 //	public static final String URL_IMAGE = "https://api.androidhive.info/volley/volley-image.jpg";
 
-    public static final String URL_USER = "https://48bbbd75-a948-45d5-8711-721c89526c8c.mock.pstmn.io/user/";
+    public static final String URL_USER_PSTMN = "https://48bbbd75-a948-45d5-8711-721c89526c8c.mock.pstmn.io/user/";
+    public static final String URL_USER_SERVER = "http://coms-309-011.class.las.iastate.edu:8080/users/";
+
+    public static final String URL_USER_SAMPLE = "https://48bbbd75-a948-45d5-8711-721c89526c8c.mock.pstmn.io/user/?username=johnrickman&password=12345678";
 }
