6f1888338203947d6672345b9d2874b5a8becafe
BagaBBG <70350040+BagaBBG@users.noreply.github.com>
2022-10-07 11:19:35 -0500

    Store class (Controller) work updates

--- Backend/GroKart Backend/groKart_v3/src/main/java/groKart_app/Stores/Store.java
+++ Backend/GroKart Backend/groKart_v3/src/main/java/groKart_app/Stores/Store.java
@@ -3,9 +3,11 @@ package groKart_app.Stores;
 import groKart_app.Items.Item;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
-/*
+
 @Entity
 @Table (name = "Stores")
 public class Store {
@@ -17,11 +19,37 @@ public class Store {
     private String storeName;
 
     @OneToMany (mappedBy = "storeName")
-    private Set<Item> items;
+    private List<Item> items;
 
 
-    public Store() {
+    public Store(int storeId, String storeName){
+        this.storeId = storeId;
+        this.storeName = storeName;
+        items = new ArrayList<>();
     }
+    public Store() {
+        items = new ArrayList<>() ;
+    }
+
+
+    public int getStoreId() {return storeId;}
+
+    public void setStoreId(int storeId) {this.storeId = storeId;}
+
+    public String getStoreName() {return storeName;}
+
+    public void setStoreName(String storeName) {this.storeName = storeName;}
+
+    public List<Item> getItems() {return items;}
+
+    public void setItems(List<Item> items) {this.items = items;}
+
+    public void addItems(Item items){
+        this.items.add(items);
+    }
+
+
+
 }
 
- */
+
--- Backend/GroKart Backend/groKart_v3/src/main/java/groKart_app/Stores/StoreController.java
+++ Backend/GroKart Backend/groKart_v3/src/main/java/groKart_app/Stores/StoreController.java
@@ -1,6 +1,36 @@
 package groKart_app.Stores;
+
 /*
+import groKart_app.Users.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class StoreController {
+    @Autowired
+    StoreRepository storeRepository;
+
+    private String success = "{\"message\":\"success\"}";
+    private String failure = "{\"message\":\"failure\"}";
+
+    /**
+     * GET ALL STORES IN THE DB
+     * @return
+     *
+    @GetMapping(path = "/stores")
+    List<Store> getAllStores(){
+        return storeRepository.findAll();
+    }
 }
 
- */
+*/
+
+
