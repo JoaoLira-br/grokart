cf46b9ea061e44df653f282a66e4fad5ebcf8cbf
Joao Lira <lira440440@gmail.com>
2022-10-28 14:26:14 -0500

    creates CreateNewListActivity

---
+++ Frontend/grokart/app/src/main/java/com/example/grokart/CreateNewListActivity.java
@@ -0,0 +1,14 @@
+package com.example.grokart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateNewListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_new_list);
+    }
+}
