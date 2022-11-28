51648f25cb6a95e6297eb61f9e313f7f71ca35d4
BagaBBG <70350040+BagaBBG@users.noreply.github.com>
2022-11-28 16:46:17 -0600

    Controller 1 update

--- Backend/groKart_v3/src/main/java/groKart_app/Reports/ReportRepository.java
+++ Backend/groKart_v3/src/main/java/groKart_app/Reports/ReportRepository.java
@@ -1,12 +1,10 @@
 package groKart_app.Reports;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-@Repository
 public interface ReportRepository extends JpaRepository<Report, Long>{
     Report findById(int id);
 
